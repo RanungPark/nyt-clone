a11fc640e8e8f8fc151332e9b033a2502498f2ed
RanungPark <ckehfqkr302@naver.com>
2023-11-15 14:58:18 +0900

    라우터 설정

---
+++ src/Router.tsx
@@ -0,0 +1,23 @@
+import {createBrowserRouter} from 'react-router-dom'
+import App from './App'
+import HomeScreen from './page/HomeScreen'
+import ScrapScreen from './page/ScrapScreen'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        path: '',
+        element: <HomeScreen />
+      },
+      {
+        path: 'scrap',
+        element: <ScrapScreen />
+      }
+    ]
+  }
+])
+
+export default router
