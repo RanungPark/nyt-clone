24421a210179d347f652202bab03b720dc52c5cc
RanungPark <ckehfqkr302@naver.com>
2023-11-15 14:58:13 +0900

    theme 설정

---
+++ src/styled.d.ts
@@ -0,0 +1,15 @@
+import 'styled-components'
+
+declare module 'styled-components' {
+  export interface DefaultTheme {
+    glay: string;
+    starColor: string;
+    bgColor: string;
+    white90: string;
+    white100: string;
+    black80: string;
+    black100: string;
+    blueSub: string;
+    blueMain: string;
+  }
+}
---
+++ src/theme.ts
@@ -0,0 +1,13 @@
+import { DefaultTheme } from 'styled-components';
+
+export const defaultTheme:DefaultTheme = {
+  glay: '#6D6D6D',
+  starColor: '#FFB800',
+  bgColor: '#F0F1F4',
+  white90: '#FEFEFE',
+  white100: '#FFFFFF',
+  black80: '#6D6D6D',
+  black100: '#000000',
+  blueSub: '#82B0F4',
+  blueMain: '#3478F6'
+}
