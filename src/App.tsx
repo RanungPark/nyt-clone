c82b12ca71397066a9aa86680e38b66643995219
RanungPark <ckehfqkr302@naver.com>
2023-11-15 15:20:56 +0900

    app 기본 스타일 적용

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,25 @@ import { Outlet } from 'react-router';
 import Banner from './components/Banner';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import {styled} from 'styled-components';
+
+const Wapper = styled.div`
+  width: 375px;
+  height: 812px;
+  border-radius: 30px;
+  background-color: ${props => props.theme.bgColor};
+  box-shadow: 0px 0px 6px 0px rgba(0,0,0,0.5);
+  overflow: hidden;
+`
 
 const App = () => {
   return (
-    <>
+    <Wapper>
       <Banner />
       <Header />
       <Outlet />
       <Footer />
-    </>
+    </Wapper>
   );
 };
 
