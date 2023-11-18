63001e5ad34e628f64ffe1c577c276b9d919eccf
RanungPark <ckehfqkr302@naver.com>
2023-11-18 14:30:20 +0900

    스타일 변경

--- src/App.tsx
+++ src/App.tsx
@@ -13,9 +13,6 @@ const Wapper = styled.div`
   box-shadow: 0px 0px 6px 0px rgba(0,0,0,0.5);
   overflow: hidden;
   position: relative;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
 `
 
 const App = () => {
