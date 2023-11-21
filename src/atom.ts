4f4ee48a6054948cd8cb10cf2095575197235040
RanungPark <ckehfqkr302@naver.com>
2023-11-21 22:13:32 +0900

    filter 관련 atom 추가

--- src/atom.ts
+++ src/atom.ts
@@ -4,3 +4,18 @@ export const isHomeScreenAtom = atom({
   key: 'isHomeScreen',
   default: true,
 })
+
+export const isHeadlineAtom = atom({
+  key: 'isHeadline',
+  default: '',
+})
+
+export const isPubDateAtom = atom({
+  key: 'isPubDate',
+  default: '',
+})
+
+export const isGlocationsAtom = atom({
+  key: 'isGlocations',
+  default: [],
+})
