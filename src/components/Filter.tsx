06ed0a495c2854fd925324f3f5b7a7c00f9f785a
RanungPark <ckehfqkr302@naver.com>
2023-11-23 01:52:03 +0900

    input 스타일링과 상태 atom과 연결

--- src/components/Filter.tsx
+++ src/components/Filter.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
-import { useLocation} from 'react-router-dom'
+import { useLocation, useNavigate} from 'react-router-dom'
 import styled from 'styled-components';
+import vector from '../svgs/Vector.svg'
+import Glocations from './Glocations';
+import { useRecoilState, useRecoilValue, useSetRecoilState } from 'recoil';
+import { isCountrysAtom, isHeadlineAtom, isPubDateAtom, isSubmitAtom } from '../atom';
 
 const Wapper = styled.div`
   position: absolute;
@@ -13,7 +17,6 @@ const Wapper = styled.div`
 `
 
 const FilterList = styled.div`
-  height: 500px;
   background-color: ${props => props.theme.white100};
   padding: 20px;
   position: absolute;
@@ -49,28 +52,103 @@ input {
   letter-spacing: -0.08em
 }
 `
+
 const PubDate = styled.div`
   width: 295px;
   display:flex;
   flex-direction: column;
   gap: 8px;
+
+  input[type='date'] {
+    position: relative;
+    border-radius:8px;
+    border: 1px solid ${props => props.theme.glay};
+    background: url(../svg/Vector.jpg) no-repeat right 20px center / 16px auto;
+    color: ${props => props.theme.glay};
+    padding: 10px 0px 10px 20px;
+    font-weight:400;
+    font-size: 14px;
+    line-height: 24px;
+    letter-spacing: -0.08em;
+  }
+
+  input[type='date']::-webkit-clear-button,
+  input[type='date']::-webkit-inner-spin-button{
+    display: none;
+  }
+
+  input[type='date']::-webkit-calendar-picker-indicator  {
+    position: absolute;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    background: transparent;
+    color: transparent;
+    cursor: pointer;
+  }
+  
+  input[type='date']::before {
+    content: attr(data-placeholder); 
+    width: 100%;
+  }
+
+  input[type='date']:valid::before {
+    display: none;
+  }
 `
-const Glocations = styled.div`
+const GlocationsBox = styled.div`
   width: 295px;
   display:flex;
   flex-direction: column;
   gap: 8px;
 `
 
+const Button = styled.button`
+  height: 60px;
+  border-radius: 16px;
+  background-color: ${props => props.theme.blueMain};
+  border: none;
+  color: ${props => props.theme.white100};
+  font-size: 16px;
+  font-weight: 600;
+  letter-spacing: -0.1em;
+  line-height: 24px;  
+  cursor: pointer;
+`
+
 const Filter = () => {
   const filterMatch = useLocation()
+  const [headline, setHeadline] = useRecoilState(isHeadlineAtom);
+  const [pubDate, setPubDate] = useRecoilState(isPubDateAtom);
+  const setSubmitDate = useSetRecoilState(isSubmitAtom)
+  const countrys = useRecoilValue(isCountrysAtom)
+
+  const navigate = useNavigate()
+
+  const handleHeadlineChagne = (e: React. ChangeEvent<HTMLInputElement>) => {
+    setHeadline(e.target.value)
+  }
+
+  const handlePubDateChagne = (e: React. ChangeEvent<HTMLInputElement>) => {
+    setPubDate(e.target.value)
+  }
+
+  const handleButtonClick = () => {
+    setSubmitDate({
+      headline,
+      pubDate,
+      countrys: {...countrys}
+    })
+    navigate('/')
+  }
+
 
   return (
     <>
     {
       filterMatch.pathname === '/filter' &&
       <Wapper>
-        <form>
           <FilterList>
             <Headline>
               <Title>
@@ -79,35 +157,33 @@ const Filter = () => {
               <input 
                 type="text" 
                 placeholder='검색하실 헤드라인을 입력해주세요.'
+                value={headline}
+                onChange={handleHeadlineChagne}
               />
             </Headline>
             <PubDate>
               <Title>
                 날짜
               </Title>
-              <input type="date" 
-
+              <input
+                id='pubDate' 
+                type="date" 
+                data-placeholder='날짜를 선택해주세요.'
+                value={pubDate}
+                onChange={handlePubDateChagne}
+                required
+                style={{background: `url(${vector}) no-repeat right 20px center / 16px auto`}}
               />
             </PubDate>
-            <Glocations>
+            <GlocationsBox>
               <Title>
                 국가
               </Title>
-                <ul>
-                  <li>대한민국</li>
-                  <li>중국</li>
-                  <li>일본</li>
-                  <li>미국</li>
-                  <li>북한</li>
-                  <li>러시아</li>
-                  <li>프랑스</li>
-                  <li>영국</li>
-                  <li>북한</li>
-                </ul>
-            </Glocations>
+              <Glocations />
+            </GlocationsBox>
+            <Button onClick={handleButtonClick}>필터 적용하기</Button>
           </FilterList>
-          </form>
-        </Wapper>
+      </Wapper>
     }
     </>
   );
