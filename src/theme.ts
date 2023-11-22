ac747bec10b76130966ab638ae50920a1f070b34
RanungPark <ckehfqkr302@naver.com>
2023-11-23 01:52:18 +0900

    white 60 색상 추가

--- src/theme.ts
+++ src/theme.ts
@@ -4,6 +4,7 @@ export const defaultTheme:DefaultTheme = {
   glay: '#6D6D6D',
   starColor: '#FFB800',
   bgColor: '#F0F1F4',
+  white60: '#F2F2F2',
   white90: '#FEFEFE',
   white100: '#FFFFFF',
   black80: '#6D6D6D',
