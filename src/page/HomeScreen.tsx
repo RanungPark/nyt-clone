d679dfe8362c6cefc8cf91776db91fdbabecbb36
RanungPark <ckehfqkr302@naver.com>
2023-11-22 23:31:49 +0900

    api 디렉토리 이동

--- src/page/HomeScreen.tsx
+++ src/page/HomeScreen.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useQuery } from 'react-query';
 import { styled } from 'styled-components';
-import { IResult, fetchArticleSearch } from '../api';
+import { IResult, fetchArticleSearch } from '../libs/api';
 import Card from '../components/Card';
 import Filter from '../components/Filter';
 
