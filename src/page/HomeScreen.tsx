4a6dded7b5912c136e93ea417d0ca8bd7c2ea90c
RanungPark <ckehfqkr302@naver.com>
2023-11-24 16:36:01 +0900

    refetch와 검색결과의 존재 유무 메시지

--- src/page/HomeScreen.tsx
+++ src/page/HomeScreen.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useQuery } from 'react-query';
 import { styled } from 'styled-components';
 import { IResult, fetchArticleSearch } from '../libs/api';
 import Card from '../components/Card';
 import Filter from '../components/Filter';
+import { useRecoilValue } from 'recoil';
+import { isSubmitAtom } from '../atom';
 
 const Wapper = styled.div`
   height: 100%;
@@ -27,8 +29,50 @@ const Cards = styled.ul`
   gap: 8px;
 `
 
+const NoData = styled.div`
+  background-color: ${props => props.theme.white90};
+  border-radius: 8px;
+  padding: 10px 20px;
+  height: 100px;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  justify-content: center;
+  
+  p {
+    font-weight: 600;
+    color: ${props => props.theme.black100};
+  }
+
+  p:last-child {
+    font-size: 12px;
+    color: ${props => props.theme.black80};
+    
+  }
+`
+
 const HomeScreen = () => {
-  const {isLoading, data} = useQuery<IResult>('article', fetchArticleSearch)
+  const submitDate = useRecoilValue(isSubmitAtom);
+  const {countrys, headline, pubDate} = submitDate
+
+  const [page, setPage] = useState(1)
+
+  const selectCountry = countrys.map((country) => (
+    country.clickState ? country.name : ''
+  ))
+  const filterCountry = selectCountry?.filter(Boolean)
+
+  const {isLoading, data, refetch} = useQuery<IResult>('article', () => fetchArticleSearch({
+    page,
+    headline,
+    pub_date: pubDate,
+    glocations: filterCountry,
+  }))
+
+  useEffect(()=>{
+    refetch()
+    
+  },[submitDate]);
 
   return (
     <Wapper>
@@ -36,12 +80,16 @@ const HomeScreen = () => {
         isLoading ? (<Loading>Loading...</Loading>) : <>
           <Filter />
           {
-            data?.response?.docs ?
+            !!data?.response?.docs.length ?
             (<Cards>
               {
-                data.response.docs.map((doc, index) => <Card doc={doc} key={index}/>)
+                data.response.docs.map((doc) => <Card doc={doc} key={doc._id}/>)
               }
-            </Cards>) : 'api를 지속적으로 많이 사용하여 잠시 기다려주세요'
+            </Cards>) : <NoData>
+            <p>검색한 결과가 존재한지 않습니다.</p>
+            <p>
+              (검색하지 않았을 경우 API를 지속해서 많이 사용하여 잠시 기다린 후 f5를 눌러주세요.)</p>
+            </NoData>
           }
         </>
       }
