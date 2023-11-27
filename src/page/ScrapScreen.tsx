beb30babfd45b96f1f49a8013dde1a220d28ef17
RanungPark <ckehfqkr302@naver.com>
2023-11-27 14:50:17 +0900

    스크랩 스크린 생성

--- src/page/ScrapScreen.tsx
+++ src/page/ScrapScreen.tsx
@@ -1,10 +1,41 @@
 import React from 'react';
+import styled from 'styled-components';
+import { useRecoilValue } from 'recoil';
+import { ScrapsState } from '../atom';
+import ScrapCard from '../components/ScrapCard';
+
+const Wrapper = styled.div`
+  height: 596px;
+  padding: 20px;
+  overflow: scroll;
+  & {
+    -ms-overflow-style: none;
+  }
+  &::-webkit-scrollbar {
+    display: none;
+  }
+`;
+
+const Cards = styled.ul`
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+`;
 
 const ScrapScreen = () => {
+  const scraps = useRecoilValue(ScrapsState);
+
   return (
-    <div>
-      scrap
-    </div>
+    <Wrapper>
+      <Cards>
+        {
+          scraps.map(scrap => <ScrapCard 
+            scrap={scrap}
+            key={scrap._id}
+          />)
+        }
+      </Cards>
+    </Wrapper>
   );
 };
 
