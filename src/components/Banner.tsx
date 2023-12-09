d749b914feaf747efab96b575f85a29d21eac0a6
RanungPark <ckehfqkr302@naver.com>
2023-12-09 16:45:32 +0900

    Revert "Revert "Revert "Revert "Prettier 수정""""
    This reverts commit 3de4ce4bbeef1b50d41895cb99409c61bbb081d1.

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import { styled } from 'styled-components';
-import time from '../svgs/Time.svg';
-import cellular from '../svgs/Cellular.svg';
-import wiFi from '../svgs/Wi-Fi.svg';
-import battery from '../svgs/Battery.svg';
+import React from "react";
+import { styled } from "styled-components";
+import time from "../svgs/Time.svg";
+import cellular from "../svgs/Cellular.svg";
+import wiFi from "../svgs/Wi-Fi.svg";
+import battery from "../svgs/Battery.svg";
 
 const Wrapper = styled.div`
   width: 100%;
@@ -11,15 +11,15 @@ const Wrapper = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-between;
-  background-color: ${props => props.theme.white100};
-`
+  background-color: ${(props) => props.theme.white100};
+`;
 
 const Time = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
   padding: 14px 0px 12px 29.5px;
-`
+`;
 
 const Remain = styled.div`
   display: flex;
@@ -27,18 +27,18 @@ const Remain = styled.div`
   justify-content: center;
   gap: 5px;
   padding: 17.16px 14.5px 15.34px 0;
-`
+`;
 
 const Banner = () => {
   return (
     <Wrapper>
       <Time>
-        <img src={time} alt='time'/>
+        <img src={time} alt="time" />
       </Time>
       <Remain>
-        <img src={cellular} alt='cellular'/>
-        <img src={wiFi} alt='wiFi'/>
-        <img src={battery} alt='battery'/>
+        <img src={cellular} alt="cellular" />
+        <img src={wiFi} alt="wiFi" />
+        <img src={battery} alt="battery" />
       </Remain>
     </Wrapper>
   );
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,15 +1,15 @@
-import React from 'react';
-import { useNavigate } from 'react-router';
-import { styled } from 'styled-components';
-import { useRecoilState } from 'recoil';
-import { isHomeScreenAtom } from '../atom';
-import home from '../svgs/Home.svg';
-import homeFill from '../svgs/HomeFill.svg';
-import scrap from '../svgs/Scrap.svg';
-import scrapFill from '../svgs/ScrapFill.svg';
+import React from "react";
+import { useNavigate } from "react-router";
+import { styled } from "styled-components";
+import { useRecoilState } from "recoil";
+import { isHomeScreenAtom } from "../atom";
+import home from "../svgs/Home.svg";
+import homeFill from "../svgs/HomeFill.svg";
+import scrap from "../svgs/Scrap.svg";
+import scrapFill from "../svgs/ScrapFill.svg";
 
 const Wrapper = styled.div`
-  background-color: ${props => props.theme.black100};
+  background-color: ${(props) => props.theme.black100};
   border-radius: 30px;
   display: flex;
   align-items: center;
@@ -19,29 +19,11 @@ const Wrapper = styled.div`
   position: absolute;
   bottom: 0;
   z-index: 100;
-`
+`;
 
-const ToHome = styled.div<{isHomeScreen: boolean}>`
-  color: ${props => props.isHomeScreen ? props.theme.white100 : props.theme.black80};
-  width: 26px;
-  height: 45px;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  gap: 9px;
-  cursor: pointer;
-
-  span {
-    font-size: 8px;
-    font-weight: 600;
-    line-height: 12px;
-    text-align: center;
-  };
-`
-
-const ToScrap = styled.div<{isHomeScreen: boolean}>`
-  color: ${props => props.isHomeScreen ? props.theme.black80 : props.theme.white100};
+const ToHome = styled.div<{ isHomeScreen: boolean }>`
+  color: ${(props) =>
+    props.isHomeScreen ? props.theme.white100 : props.theme.black80};
   width: 26px;
   height: 45px;
   display: flex;
@@ -57,39 +39,59 @@ const ToScrap = styled.div<{isHomeScreen: boolean}>`
     line-height: 12px;
     text-align: center;
   }
-`
+`;
+
+const ToScrap = styled.div<{ isHomeScreen: boolean }>`
+  color: ${(props) =>
+    props.isHomeScreen ? props.theme.black80 : props.theme.white100};
+  width: 26px;
+  height: 45px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 9px;
+  cursor: pointer;
+
+  span {
+    font-size: 8px;
+    font-weight: 600;
+    line-height: 12px;
+    text-align: center;
+  }
+`;
 
 const Footer = () => {
-  const [isHomeScreen, setIsHomeScreen] = useRecoilState(isHomeScreenAtom)
-  const navigate = useNavigate()
+  const [isHomeScreen, setIsHomeScreen] = useRecoilState(isHomeScreenAtom);
+  const navigate = useNavigate();
 
   const handleChangeHomeScreen = () => {
-    navigate('/nyt-clone')
-    setIsHomeScreen(true)
-  } 
+    navigate("/nyt-clone");
+    setIsHomeScreen(true);
+  };
 
   const handleChangeScrapScreen = () => {
-    navigate('/nyt-clone/scrap')
-    setIsHomeScreen(false)
-  } 
+    navigate("/nyt-clone/scrap");
+    setIsHomeScreen(false);
+  };
 
   return (
     <Wrapper>
       <ToHome onClick={handleChangeHomeScreen} isHomeScreen={isHomeScreen}>
-        {
-          isHomeScreen ? <img src={homeFill} alt='homeFill'/> : <img src={home} alt='home'/>
-        }
-        <span>
-          홈
-        </span>
+        {isHomeScreen ? (
+          <img src={homeFill} alt="homeFill" />
+        ) : (
+          <img src={home} alt="home" />
+        )}
+        <span>홈</span>
       </ToHome>
       <ToScrap onClick={handleChangeScrapScreen} isHomeScreen={isHomeScreen}>
-        {
-          isHomeScreen ? <img src={scrap} alt='scrap'/> : <img src={scrapFill} alt='scrapFill'/>
-        }
-        <span>
-          스크랩
-        </span>
+        {isHomeScreen ? (
+          <img src={scrap} alt="scrap" />
+        ) : (
+          <img src={scrapFill} alt="scrapFill" />
+        )}
+        <span>스크랩</span>
       </ToScrap>
     </Wrapper>
   );
